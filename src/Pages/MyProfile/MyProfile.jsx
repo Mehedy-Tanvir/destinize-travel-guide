e35f7c983a3cf6579ad3da77ffdec8087ef8313c
Mehedy-Tanvir <mehedytanvir451@gmail.com>
2023-11-26 23:27:22 +0600

    story adding functionality created

--- src/Pages/MyProfile/MyProfile.jsx
+++ src/Pages/MyProfile/MyProfile.jsx
@@ -1,12 +1,28 @@
 import { useQuery } from "@tanstack/react-query";
 import useUtils from "../../Utils/useUtils";
 import useAuth from "../../Hooks/useAuth";
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
+import toast from "react-hot-toast";
 
 const MyProfile = () => {
   const { user } = useAuth();
   const { getMyProfile } = useUtils();
+  const axiosSecure = useAxiosSecure();
   const handleSubmit = (e) => {
     e.preventDefault();
+    const form = e.target;
+    const storyImage = form.storyImage.value;
+    const storyDetails = form.storyDetails.value;
+    const story = { storyDetails, storyImage };
+    axiosSecure
+      .post("/stories", story)
+      .then((res) => {
+        console.log(res.data);
+        toast.success("Story added");
+        form.storyImage.value = "";
+        form.storyDetails.value = "";
+      })
+      .catch((error) => console.log(error));
   };
 
   // Queries
@@ -19,8 +35,6 @@ const MyProfile = () => {
     queryKey: ["myProfile", user?.email],
     queryFn: getMyProfile,
   });
-
-  console.log(myProfile);
 
   return (
     !isLoading &&
@@ -48,15 +62,22 @@ const MyProfile = () => {
           Add Your <span className="text-[#4475F2]">Story</span>
         </h1>
         <form onSubmit={handleSubmit} className="w-full card-body">
-          <div className="">
+          <div className="flex flex-col gap-2">
+            <div className="form-control">
+              <input
+                name="storyImage"
+                type="text"
+                placeholder="Image URL"
+                className="input input-bordered"
+                required
+              />
+            </div>
             <div className="form-control">
               <textarea
-                className="textarea textarea-bordered"
+                className="w-full max-w-xs textarea textarea-bordered textarea-lg"
                 placeholder="Your Story"
                 required
-                name="story"
-                cols="5"
-                rows="4"
+                name="storyDetails"
               ></textarea>
             </div>
           </div>
