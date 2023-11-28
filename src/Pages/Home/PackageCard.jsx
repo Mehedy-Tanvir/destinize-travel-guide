edd909dbd90f671270930fb4a63cfb6501c9e42a
Mehedy-Tanvir <mehedytanvir451@gmail.com>
2023-11-28 15:05:45 +0600

    functionaliy created for adding to wishlist

--- src/Pages/Home/PackageCard.jsx
+++ src/Pages/Home/PackageCard.jsx
@@ -1,8 +1,41 @@
 import { Link } from "react-router-dom";
 import { FaHeart } from "react-icons/fa";
 import PropTypes from "prop-types";
+import useAuth from "../../Hooks/useAuth";
+import useUtils from "../../Utils/useUtils";
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
+import toast from "react-hot-toast";
 
 const PackageCard = ({ tourPackage }) => {
+  const { user, loading } = useAuth();
+  const { getMyProfile } = useUtils();
+  const axiosSecure = useAxiosSecure();
+  // Queries
+  const {
+    data: myProfile,
+    refetch,
+    isLoading,
+  } = useQuery({
+    queryKey: ["myProfile", user?.email],
+    queryFn: getMyProfile,
+  });
+  const handleWish = () => {
+    const WishlistItem = {
+      tourist: myProfile?._id,
+      tourPackage: tourPackage?._id,
+    };
+    axiosSecure
+      .post("/wishlistItems", WishlistItem)
+      .then((res) => {
+        console.log(res.data);
+        toast.success("Successfully added to your wishlist");
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error("Unable to add to your wishlist");
+      });
+  };
   return (
     <div className="flex w-[300px] justify-self-center self-center h-[430px] border-2 p-[20px] rounded-3xl flex-col items-center gap-4 shadow-lg">
       <div className="relative">
@@ -11,11 +44,13 @@ const PackageCard = ({ tourPackage }) => {
           src={tourPackage?.galleryImages[0]}
           alt=""
         />
-        <div className="absolute bottom-0 left-0 w-full h-[30%] bg-white hover:opacity-75 opacity-80 flex justify-center">
-          <button>
-            <FaHeart className="text-4xl text-blue-500 hover:opacity-90" />
-          </button>
-        </div>
+        {!loading && user && (
+          <div className="absolute bottom-0 left-0 w-full h-[30%] bg-white hover:opacity-75 opacity-80 flex justify-center">
+            <button onClick={handleWish}>
+              <FaHeart className="text-4xl text-blue-500 hover:opacity-90" />
+            </button>
+          </div>
+        )}
       </div>
       <div className="flex-grow text-left">
         <h1 className="text-xl font-semibold">{tourPackage?.tripTitle}</h1>
